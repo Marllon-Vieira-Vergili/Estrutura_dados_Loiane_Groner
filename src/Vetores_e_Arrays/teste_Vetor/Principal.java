d4bd640cd3d3c37fe67d01cf3c900f3e0bb028ab
Marllon Vieira Vergili <marllon@cooxupe.com.br>
2024-10-28 17:16:00 -0300

    Adicionando método para adição de elementos na Lista

---
+++ src/Vetores_e_Arrays/teste_Vetor/Principal.java
@@ -0,0 +1,15 @@
+package Vetores_e_Arrays.teste_Vetor;
+
+import Vetores_e_Arrays.Vetor;
+
+public class Principal {
+    public static void main(String[] args) throws Exception {
+
+        Vetor vetor = new Vetor(2);
+
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+
+
+    }
+}
